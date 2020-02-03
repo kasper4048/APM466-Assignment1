65a45ca547d701e40233e56bc59cd6d959f7be5b
kasper4048 <60114396+kasper4048@users.noreply.github.com>
2020-02-02 23:17:32 -0500

    Add files via upload

--- Assignment 1 APM466.py
+++ Assignment 1 APM466.py
@@ -33,7 +33,51 @@ def compile_days(days):
         all_yields.append(repeat_step(bonds, i))
     return all_yields
         
-    
+def spot_rates(bonds):
+    spot_rates = []
+    for x in range(1,10):
+        spots = []
+        for i in range(2, 10):
+            exponent = (1 / (i * 0.5))
+            new_spot = ((100 / bonds[12 * i - x]) ** exponent) - 1
+            spots.append(new_spot)
+        spot_rates.append(spots)
+    return spot_rates
+
+def forward_rates(bonds):
+    forward_rates = []
+    spots = spot_rates(bonds)
+    for i in range(1,10):
+        forwards = []
+        #index from 0 to 4 since we're using spot rates, which start at year 1
+        for x in range(0,4):
+            new_forward = ((1 + spots[i-1][2*x])**(x+1)) / ((1+spots[i-1][0])**1) - 1
+            forwards.append(new_forward)
+        forward_rates.append(forwards)
+    return forward_rates
+            
+def cov_matrix_yield(bonds):
+    cov_matrix_yield = []
+    yields = compile_days(10)
+    for i in range(1,5):
+        matrix_yields = []
+        for x in range(1,5):
+            matrix_yields.append(round(numpy.log(yields[i][x+1]/yields[i][x]),4))
+        cov_matrix_yield.append(matrix_yields)
+    return cov_matrix_yield
+
+def cov_matrix_forward(bonds):
+    cov_matrix_forward = []
+    forwards = forward_rates(bonds)
+    for i in range(0,2):
+        matrix_forwards = []
+        #can only index over 1 to 3, since the entire vector of forward rates is 
+        #only from 0 to 3, but at 0 for each vector, the value is 0, and we cannot
+        #divide by 0, we omit 1yt-1yr
+        for x in range(1,3):
+            matrix_forwards.append(round(numpy.log(abs(forwards[i][x+1]/forwards[i][x])),4))
+        cov_matrix_forward.append(matrix_forwards)
+    return cov_matrix_forward
 if __name__ == '__main__':
     running = True
     while running:
@@ -44,16 +88,42 @@ if __name__ == '__main__':
             bond_file = open('bond_data1.csv')
             bonds = csv_to_list(bond_file)
             graph_data = compile_days(10)
-            print(graph_data)
             x = [0.5,1,1.5,2,2.5,3,3.5,4,4.5,5] 
             fig = pyplot.figure()
             ax = pyplot.subplot(111)  
             pyplot.ylabel('Yields')
             pyplot.xlabel('Time (Years)')              
-            for i in range(1,9):
+            for i in range(0,9):
                 y = graph_data[i]
-                ax.plot(x,y, label = 'day {}'.format(i))
+                ax.plot(x,y, label = 'day {}'.format(i + 1))
                 ax.legend()
             pyplot.show()
             pyplot.savefig('yield_curve.png', dpi = 600)
+            
+            spot_graph_data = spot_rates(bonds)
+            fig = pyplot.figure()
+            ax = pyplot.subplot(111)
+            pyplot.ylabel('Spot Rates')
+            pyplot.xlabel('Time to Maturity (Years)')
+            x = [1.5,2,2.5,3,3.5,4,4.5,5]
+            for i in range(0,9):
+                y = spot_graph_data[i]
+                ax.plot(x,y, label = 'day {}'.format(i+1))
+                ax.legend()
+            pyplot.show()
+            pyplot.savefig('spot_curve.png', dpi = 600)
+            
+            forward_graph_data = forward_rates(bonds)
+            fig = pyplot.figure()
+            ax = pyplot.subplot(111)
+            pyplot.ylabel('Forward Rates')
+            pyplot.xlabel('Rate Term (starting at 1 year)')
+            x = [1,2,3,4]
+            for i in range(0,9):
+                y = forward_graph_data[i]
+                ax.plot(x,y, label = 'day {}'.format(i+1))
+                ax.legend()
+            pyplot.show()
+            pyplot.savefig('forward_curve.png', dpi = 600)
+            
             
